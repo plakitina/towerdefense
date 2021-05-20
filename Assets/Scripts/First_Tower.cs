9411e7aa7f6333824710a079beafe75a7108aa9f
TestyEldrich <olzhas2106@gmail.com>
2021-05-20 12:07:51 +0600

    svoe

---
+++ Assets/Scripts/First_Tower.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class First_Tower : MonoBehaviour
+{
+    [Header("Details")]
+    public int Cost;
+    public int Damage;
+    public float AttackSpeed;
+    public bool isMultipleTarget;
+    public bool flyingEnemyTarget;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Tower_buttons.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Tower_buttons : MonoBehaviour
+{
+    [SerializeField]
+    GameObject object_Tower;
+
+    public GameObject Object_Tower
+    {
+        get
+        {
+            return object_Tower;
+        }
+    }
+}
