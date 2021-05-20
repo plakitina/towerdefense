ad2244f52c7f57aeed2a3c7fc99a5b82a7687aab
plakitina <kairbeknurbai@mail.ru>
2021-05-20 11:52:49 +0600

    залил по новому врагов

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int target = 0;
+    public Transform exit;
+    public Transform[] wayPoints;
+    public float navigation;
+
+    Transform enemy;
+    float navigationTime = 0;
+
+    void Start()
+    {
+        enemy = GetComponent<Transform>();
+
+    }
+
+    void Update()
+    {
+        if(wayPoints != null)
+        {
+            navigationTime += Time.deltaTime;
+            if(navigationTime > navigation)
+            {
+                if(target < wayPoints.Length)
+                {
+                    enemy.position = Vector2.MoveTowards(enemy.position, wayPoints[target].position, navigationTime);
+                    navigationTime = 0;
+                }
+                else
+                {
+                    enemy.position = Vector2.MoveTowards(enemy.position, exit.position, navigationTime);
+                    navigationTime = 0;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "MovingPoint")
+        {
+            target += 1;
+        }
+        else if(collision.tag == "Finish")
+        {
+            Manager.instance.removeEnemyFromScreen();
+            Destroy(gameObject);
+        }
+    }
+}
